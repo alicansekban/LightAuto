62079f007657a277c98c6b6495277d8dce062c91
alicansekban <alicansekban@hotmail.com>
2025-01-11 16:28:16 +0300

    added libraries

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,12 +1,14 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 kotlin {
@@ -27,22 +29,58 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     sourceSets {
-        
+
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            // koin android
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
+
+            // ktor android
+            implementation(libs.ktor.client.android)
+            implementation(libs.ktor.client.okhttp)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+
+            // lifecycle
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            // coroutines
+
+            implementation(libs.kotlinx.coroutines.core)
+
+            //ktor
+            implementation(libs.bundles.ktor)
+
+            // koin
+            api(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.lifecycle.viewmodel)
+
+            //navigation
+            implementation(libs.navigation.compose)
+
+            // room db
+
+            implementation(libs.room.runtime)
+            implementation(libs.sqlite.bundled)
+
+        }
+        iosMain.dependencies {
+            // ktor ios
+            implementation(libs.ktor.client.darwin)
         }
     }
 }
@@ -77,4 +115,14 @@ android {
 dependencies {
     debugImplementation(compose.uiTooling)
 }
+
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
+dependencies {
+    implementation(libs.androidx.material3.android)
+    ksp(libs.room.compiler)
+}
+
 
